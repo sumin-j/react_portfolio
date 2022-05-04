c48ff776bb65fd14fadf55df69c0ff72af949e08
sumin-j <soommmni@gmail.com>
2022-05-04 11:48:02 +0900

    서브페이지 공통레이아웃 및 반응형 제작

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function Department() {
   return (
-    <div>
+    <section className="content">
+      <figure></figure>
+      <div className="inner">
         <h1>Department</h1>
-    </div>
+      </div>
+    </section>
   );
 }
 
