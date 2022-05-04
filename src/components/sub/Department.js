e76352d2a5a72ba56aad951daeb2fc75bb16be73
sumin-j <soommmni@gmail.com>
2022-05-04 14:37:48 +0900

    useState 주석설명

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -2,6 +2,9 @@ import Layout from '../common/Layout';
 import {useEffect, useState} from 'react';
 
 function Department() {
+  // [상태값, 상태값을 변경하는 함수] = useState(상택초기값)
+  // 스테이트값을 변경해야 될때 set함수로 변경을 해야지만 화면의 재렌더링 일어남
+  // 스테이트 변경함수로 상태값을 변경하지 않으면 리액트는 변경된 요소를 인지할 수 없기때문에 화면 갱신이 안됨(상태값 관리안됨)
   let [num, setNum] = useState(0);
 
   //현재 useEffet의 의존성 배열은 비어있으므로
@@ -37,9 +40,9 @@ export default Department;
   -컴포넌트 생성시 한번만 실행
   --useEffect의 의존성 배열이 비어있을때 
 
-  -컴포넌트 소멸시 한번말 실행
+  -컴포넌트 소멸시 한번만 실행
   --useEffect의 의존성 배열이 비어있고 함수를 리턴으로 내보낼때 
 
   -컴포넌트가 변경될때마다 실행
-  --useEffect에 특정 state값을 의존성 뱅려에 집어넣음
+  --useEffect에 특정 state값을 의존성 배열에 집어넣음
 */
